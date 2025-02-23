5f63948fc1ff86f25c0db631ddf5955757fbdb11
NguyenDuc45 <nguyenduc04052002@gmail.com>
2025-02-23 21:57:26 +0700

    Merge Liêm

--- resources/views/admins/danhmucs/index.blade.php
+++ resources/views/admins/danhmucs/index.blade.php
@@ -35,7 +35,7 @@
                 <div class="title-header option-title">
                     <h5>Quản lý danh mục</h5>
                     <form class="d-inline-flex">
-                        <a href="{{ route('danhmucs.create',1) }}" class="align-items-center btn btn-theme d-flex">
+                        <a href="{{ route('danhmucs.create', 1) }}" class="align-items-center btn btn-theme d-flex">
                             <i data-feather="plus"></i>Thêm mới
                         </a>
                     </form>
@@ -43,25 +43,21 @@
 
                 <div class="table-responsive category-table">
                     @if (session('success'))
-                                  
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                      <strong> {{session('success')}} </strong> 
-                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  </div>
-                        
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong> {{ session('success') }} </strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
                     @endif
                     @if (session('error'))
-                        
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                      <strong> {{session('error')}} </strong> 
-                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                  </div>
-                        
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong> {{ session('error') }} </strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
                     @endif
                     <div>
                         <table class="table all-package theme-table" id="table_id">
                             <thead>
-           
+
 
                                 <tr>
                                     <th>STT</th>
@@ -72,18 +68,19 @@
                             </thead>
 
                             <tbody>
- @foreach ($danhMucs as $index => $danhMuc)
+                                @foreach ($danhMucs as $index => $danhMuc)
                                     <tr>
                                         <td>{{ $index + 1 }}</td>
 
                                         <td>{{ $danhMuc->ten_danh_muc }}</td>
-                            
+
                                         <td>
                                             <div class="table-image">
-                                                <img src="{{Storage::url($danhMuc->anh_danh_muc)}}" class="img-thumbnail" alt="Hình ảnh" width="100px">
+                                                <img src="{{ Storage::url($danhMuc->anh_danh_muc) }}" class="img-thumbnail"
+                                                    alt="Hình ảnh" width="100px">
                                             </div>
                                         </td>
-                            
+
                                         <td>
                                             <ul>
                                                 <li>
@@ -91,25 +88,26 @@
                                                         <i class="ri-pencil-line"></i>
                                                     </a>
                                                 </li>
-                            
+
                                                 <li>
-                                                    <form action="{{ route('danhmucs.destroy', $danhMuc->id) }}" method="POST"
-                                                          onsubmit="return confirm('Bạn có chắc chắn muốn xóa danh mục này không?');">
+                                                    <form action="{{ route('danhmucs.destroy', $danhMuc->id) }}"
+                                                        method="POST"
+                                                        onsubmit="return confirm('Bạn có chắc chắn muốn xóa danh mục này không?');">
                                                         @csrf
                                                         @method('DELETE')
-                                                        <button type="submit" style="border: none; background: none; cursor: pointer;">
+                                                        <button type="submit"
+                                                            style="border: none; background: none; cursor: pointer;">
                                                             <i class="ri-delete-bin-line text-danger"></i>
                                                         </button>
                                                     </form>
                                                 </li>
-                                                
+
                                             </ul>
                                         </td>
                                     </tr>
-                                    
                                 @endforeach
                             </tbody>
-                            
+
                         </table>
                     </div>
                 </div>
