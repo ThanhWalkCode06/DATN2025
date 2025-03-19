e2108c04145341ee948af803b33ae1565fd18b84
liem499 <ngliem2408@gmail.com>
2025-03-19 21:55:08 +0700

    lấy về đi đức

--- app/Http/Controllers/Clients/SanPhamController.php
+++ app/Http/Controllers/Clients/SanPhamController.php
@@ -37,7 +37,7 @@ class SanPhamController extends Controller
             }
         }
 
-        $sanPhams = $query->paginate(12);
+        $sanPhams = $query->paginate(50);
 
 
         $danhMucs = DanhMucSanPham::withCount([
@@ -51,45 +51,53 @@ class SanPhamController extends Controller
 
 
 
-    public function chiTiet()
+    public function chiTiet($id)
     {
-        return view('clients.sanphams.chitiet');
-    }
-
-    public function sanPhamYeuThich()
-    {
-        $user = Auth::user();
-        return view('clients.sanphams.sanphamyeuthich', compact('user'));
-    }
-
-    public function addsanPhamYeuThich(string $id)
-    {
-        $user = Auth::user();
-        if ($user) {
-            $user->sanPhamYeuThichs()->attach($id);
-        } else {
-            return redirect()->back()->with(['error' => 'Vui lòng đăng nhập để sử dụng tính năng']);
-        }
-        return view('clients.sanphams.sanphamyeuthich', compact('user'));
-    }
-
-    public function xoaYeuThich($id)
-    {
-        try {
-            $user = Auth::user();
-
-            // Kiểm tra xem sản phẩm có trong danh sách yêu thích không
-            if (!$user->sanPhamYeuThichs()->where('san_pham_id', $id)->exists()) {
-                return response()->json(['success' => false, 'message' => 'Sản phẩm không tồn tại!'], 404);
-            }
-
-            // Xóa sản phẩm yêu thích
-            $user->sanPhamYeuThichs()->detach($id);
-
-            return response()->json(['success' => true, 'message' => 'Xóa thành công!']);
-        } catch (\Exception $e) {
-            \Log::error('Lỗi xóa sản phẩm yêu thích: ' . $e->getMessage());
-            return response()->json(['success' => false, 'message' => 'Lỗi server!'], 500);
+        $sanPhams = SanPham::with(['bienThes', 'anhSP'])->findOrFail($id);  
+        return view('clients.sanphams.chitiet', [  
+            'sanPhams' => $sanPhams,  
+            'bienThes' => $sanPhams->bienThes,  
+            'anhSPs' => $sanPhams->anhSP, // Thêm biến anhSPs để truyền vào view  
+        ]);   
+          
+            return view('clients.sanphams.chitiet');
         }
     }
-}
+
+    // public function sanPhamYeuThich()
+    // {
+    //     $user = Auth::user();
+    //     return view('clients.sanphams.sanphamyeuthich', compact('user'));
+    // }
+
+    // public function addsanPhamYeuThich(string $id)
+    // {
+    //     $user = Auth::user();
+    //     if ($user) {
+    //         $user->sanPhamYeuThichs()->attach($id);
+    //     } else {
+    //         return redirect()->back()->with(['error' => 'Vui lòng đăng nhập để sử dụng tính năng']);
+    //     }
+    //     return view('clients.sanphams.sanphamyeuthich', compact('user'));
+    // }
+
+    // public function xoaYeuThich($id)
+    // {
+    //     try {
+    //         $user = Auth::user();
+
+    //         // Kiểm tra xem sản phẩm có trong danh sách yêu thích không
+    //         if (!$user->sanPhamYeuThichs()->where('san_pham_id', $id)->exists()) {
+    //             return response()->json(['success' => false, 'message' => 'Sản phẩm không tồn tại!'], 404);
+    //         }
+
+    //         // Xóa sản phẩm yêu thích
+    //         $user->sanPhamYeuThichs()->detach($id);
+
+    //         return response()->json(['success' => true, 'message' => 'Xóa thành công!']);
+    //     } catch (\Exception $e) {
+    //         \Log::error('Lỗi xóa sản phẩm yêu thích: ' . $e->getMessage());
+    //         return response()->json(['success' => false, 'message' => 'Lỗi server!'], 500);
+    //     }
+    // }
+
