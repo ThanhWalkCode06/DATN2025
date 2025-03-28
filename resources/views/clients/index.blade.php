863d2b21587425cbe6f707e5d870bb8831559e4b
ThanhWalkCode06 <thanhnguyen062004@gmail.com>
2025-03-28 17:24:50 +0700

    add link to banner client index

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -45,7 +45,7 @@
                                 </h1>
                                 <p class="w-75 d-none d-sm-block">Những bộ quần áo thể thao thoải mái, phong cách giúp bạn
                                     tự tin vận động mỗi ngày.</p>
-                                <button onclick="location.href = 'shop-left-sidebar.html';"
+                                <button onclick="location.href = '/clientsanpham/?danh_muc_id=7';"
                                     class="btn btn-animation mt-xxl-4 mt-2 home-button mend-auto">Mua ngay <i
                                         class="fa-solid fa-right-long icon"></i></button>
                             </div>
@@ -65,7 +65,7 @@
                                         <h3 class="theme-color">Bộ Sưu Tập Áo Hoodio</h3>
                                         <p class="w-75">Chúng tôi mang đến những bộ trang phục thể thao chất lượng, thoải
                                             mái và phong cách.</p>
-                                        <a href="shop-left-sidebar.html" class="shop-button">Mua ngay <i
+                                        <a href='/clientsanpham/?danh_muc_id=5' class="shop-button">Mua ngay <i
                                                 class="fa-solid fa-right-long"></i></a>
                                     </div>
 
@@ -83,7 +83,7 @@
                                         <h4 class="text-danger">Bộ Sưu Tập Mới</h4>
                                         <p class="organic">Bắt đầu ngày mới với những bộ trang phục
                                             thoải mái.</p>
-                                        <a href="shop-left-sidebar.html" class="shop-button">Mua ngay <i
+                                        <a href='/clientsanpham/?danh_muc_id=6' class="shop-button">Mua ngay <i
                                                 class="fa-solid fa-right-long"></i></a>
                                     </div>
 
@@ -112,7 +112,7 @@
                                         <h3 class="text-uppercase fw-normal"><span class="theme-color fw-bold">Sản
                                                 Phẩm</span> Mới Nhất</h3>
                                         <h3 class="fw-light">Cập Nhật Liên Tục</h3>
-                                        <button onclick="location.href = 'shop-left-sidebar.html';"
+                                        <button onclick="location.href = '/clientsanpham';"
                                             class="btn btn-animation btn-md mend-auto">Mua Ngay <i
                                                 class="fa-solid fa-arrow-right icon"></i></button>
                                     </div>
