d09de9f1b34e0398d534163e04ab96881c7f0feb
Phuc <phucntph48072@fpt.edu.vn>
2025-04-01 22:54:55 +0700

    sửa trang phiếu giảm giá

--- resources/views/admins/phieugiamgias/edit.blade.php
+++ resources/views/admins/phieugiamgias/edit.blade.php
@@ -26,6 +26,107 @@ Chỉnh sửa mã giảm giá
 @endsection
 
 @section('content')
+<div class="col-12">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="title-header option-title">
+                        <h5>Chỉnh sửa mã giảm giá</h5>
+                    </div>
+
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel">
+                            <form class="theme-form theme-form-2 mega-form" action="{{ route('phieugiamgias.update', $phieuGiamGia->id) }}" method="POST">
+                                @csrf
+                                @method('PUT')
+
+                                <div class="row">
+                                    <!-- Tên phiếu giảm giá -->
+                                    <div class="mb-4 row align-items-center">
+                                        <label class="form-label-title col-lg-2 col-md-3 mb-0">Tên phiếu giảm giá</label>
+                                        <div class="col-md-9 col-lg-10">
+                                            <input class="form-control @error('ten_phieu') @enderror" type="text" name="ten_phieu" value="{{ old('ten_phieu', $phieuGiamGia->ten_phieu) }}">
+                                            @error('ten_phieu')
+                                            <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <!-- Mã Giảm giá -->
+                                    <div class="mb-4 row align-items-center">
+                                        <label class="col-lg-2 col-md-3 col-form-label form-label-title">Mã Giảm giá</label>
+                                        <div class="col-md-9 col-lg-10">
+                                            <input class="form-control @error('ma_phieu') @enderror" type="text" name="ma_phieu" value="{{ old('ma_phieu', $phieuGiamGia->ma_phieu) }}">
+                                            @error('ma_phieu')
+                                            <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <!-- Ngày bắt đầu -->
+                                    <div class="mb-4 row align-items-center">
+                                        <label class="col-lg-2 col-md-3 col-form-label form-label-title">Ngày bắt đầu</label>
+                                        <div class="col-md-9 col-lg-10">
+                                            <input class="form-control @error('ngay_bat_dau') @enderror" type="date" name="ngay_bat_dau" value="{{ old('ngay_bat_dau', $phieuGiamGia->ngay_bat_dau) }}">
+                                            @error('ngay_bat_dau')
+                                            <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <!-- Ngày kết thúc -->
+                                    <div class="mb-4 row align-items-center">
+                                        <label class="col-lg-2 col-md-3 col-form-label form-label-title">Ngày kết thúc</label>
+                                        <div class="col-md-9 col-lg-10">
+                                            <input class="form-control @error('ngay_ket_thuc') @enderror" type="date" name="ngay_ket_thuc" value="{{ old('ngay_ket_thuc', $phieuGiamGia->ngay_ket_thuc) }}">
+                                            @error('ngay_ket_thuc')
+                                            <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <!-- Giá trị giảm giá -->
+                                    <div class="mb-4 row align-items-center">
+                                        <label class="col-lg-2 col-md-3 col-form-label form-label-title">Giá trị giảm giá</label>
+                                        <div class="col-md-9 col-lg-10">
+                                            <input class="form-control @error('gia_tri') @enderror" type="number" name="gia_tri" step="0.01" value="{{ old('gia_tri', $phieuGiamGia->gia_tri) }}">
+                                            @error('gia_tri')
+                                            <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <!-- Trạng thái -->
+                                    <div class="mb-4 row align-items-center">
+                                        <label class="col-lg-2 col-md-3 col-form-label form-label-title">Trạng thái</label>
+                                        <div class="col-lg-10 col-md-9 d-flex gap-3">
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" id="trang_thai_kich_hoat" name="trang_thai" value="1" {{ old('trang_thai', $phieuGiamGia->trang_thai) == 1 ? 'checked' : '' }}>
+                                                <label for="trang_thai_kich_hoat" class="form-check-label ms-1">Kích hoạt</label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" id="trang_thai_khong_kich_hoat" name="trang_thai" value="0" {{ old('trang_thai', $phieuGiamGia->trang_thai) == 0 ? 'checked' : '' }}>
+                                                <label for="trang_thai_khong_kich_hoat" class="form-check-label ms-1">Không kích hoạt</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-4 d-flex gap-3">
+                                        <a href="{{ route('phieugiamgias.index') }}" class="btn btn-light">Quay lại</a>
+                                        <button type="submit" class="btn btn-solid" id="update-button" disabled>Cập Nhật Mã Giảm Giá</button>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 
